6af09db3716455117c1adeb8761358c373ccd9b8
Gil Balsiger <balsigergil@users.noreply.github.com>
2017-08-14 09:56:53 +0200

    Fix #1 Add comments in english

--- README.md
+++ README.md
@@ -8,12 +8,21 @@ Localbox is a simple vagrant file which allows you to deploy a full WAMP/LAMP/MA
 * Apache 2
 * Composer
 
+### PHP extensions
+* MySQL
+* GD
+* Xdebug
+* mbstring
+* mcrypt
+* etc...
+
 ## Requirements
-Localbox is compatible Windows, MacOS and Linux. You need only 2 things to get started with localbox: [VirtualBox](https://www.virtualbox.org/) (min 2.1.26) and [Vagrant](https://www.vagrantup.com/) (min 1.9.5).
+Localbox is compatible with Windows, MacOS and Linux. You need only 2 things to get started with it: [VirtualBox](https://www.virtualbox.org/) (min v2.1.26) and [Vagrant](https://www.vagrantup.com/) (min v1.9.5).
 
 ## Get started
 1. Clone or download the repository somewhere on your computer
 2. Go with you terminal in that folder (with the Vagrantfile) and use `vagrant up` to start the VM
 3. (Optional) Edit your hosts file and add this line: `192.168.33.10 local.dev`
 4. Go with you web browser on `192.168.33.10` or `local.dev`
-5. :thumbsup: Happy coding...
+5. The MySQL root password is `root`
+6. :thumbsup: Happy coding...
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,20 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
-echo "#########################################"
-echo "## Installation d'Apache, PHP et MySQL ##"
-echo "#########################################"
+echo "########################################"
+echo "## Apache, PHP and MySQL installation ##"
+echo "########################################"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -qy apache2 libapache2-mod-php php-mcrypt php-gd php-mbstring php-mysql php-xdebug mysql-server
 
-echo "############################"
-echo "## Configuration d'Apache ##"
-echo "############################"
+echo "        "
+echo "Installations done"
+echo "        "
+
+echo "##########################"
+echo "## Apache Configuration ##"
+echo "##########################"
 
 echo "<VirtualHost *:80>
 	ServerName local.dev
@@ -30,25 +34,37 @@ a2ensite 010-localbox
 a2dissite 000-default
 a2enmod rewrite
 
-echo "##########################"
-echo "## Configuration de PHP ##"
-echo "##########################"
+echo "        "
+echo "Apache configuration done"
+echo "        "
+
+echo "#######################"
+echo "## PHP Configuration ##"
+echo "#######################"
 
 echo "display_errors=On
 xdebug.show_local_vars=1" > /etc/php/7.0/apache2/conf.d/zzzz-custom.ini
 
-echo "##############################"
-echo "## Installation de Composer ##"
-echo "##############################"
+echo "        "
+echo "PHP configuration done"
+echo "        "
+
+echo "###########################"
+echo "## Composer Installation ##"
+echo "###########################"
 
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 service apache2 restart
 
-echo "############################"
-echo "## Configuration de MySQL ##"
-echo "############################"
+echo "        "
+echo "Composer installation done"
+echo "        "
+
+echo "#########################"
+echo "## MySQL Configuration ##"
+echo "#########################"
 
 if [ ! -f .mysql ]; then
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
@@ -56,3 +72,16 @@ fi
 touch .mysql
 
 service mysql restart
+
+echo "        "
+echo "MySQL configuration done"
+echo "        "
+
+echo " _       ___     __   ____  _      ____    ___   __ __ ";
+echo "| |     /   \   /  ] /    || |    |    \  /   \ |  |  |";
+echo "| |    |     | /  / |  o  || |    |  o  )|     ||  |  |";
+echo "| |___ |  O  |/  /  |     || |___ |     ||  O  ||_   _|";
+echo "|     ||     /   \_ |  _  ||     ||  O  ||     ||     |";
+echo "|     ||     \     ||  |  ||     ||     ||     ||  |  |";
+echo "|_____| \___/ \____||__|__||_____||_____| \___/ |__|__|";
+echo "                                        by Gil Balsiger";
